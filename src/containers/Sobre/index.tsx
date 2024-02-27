ac8d0a434172688d6a1772b231b5dcfd59f1148d
¨isaias <isaiasrocha1626@gmail.com>
2024-02-27 19:54:19 -0300

    construindo a estrutura e aseção sobre mim

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas,
+      doloremque, reiciendis modi autem magni atque recusandae, itaque
+      perferendis molestias fuga rem quod aperiam! Beatae reiciendis perferendis
+      nam. Quidem, aliquid libero.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=isaias1626&show_icons=true&theme=dark&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=isaias1626&layout=compact&langs_count=15&theme=dark" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
---
+++ src/containers/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/isaias1626.png" />
+    <Titulo fontSize={20}>Isaias Rocha</Titulo>
+  </aside>
+)
+
+export default Sidebar
